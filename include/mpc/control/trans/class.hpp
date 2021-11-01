e7dccf124971b78b0e1cb688c910772d3b959c51
acd1034 <48613285+acd1034@users.noreply.github.com>
2021-11-01 17:30:14 +0900

    Add `monad_trans`

---
+++ include/mpc/control/trans/class.hpp
@@ -0,0 +1,35 @@
+/// @file class.hpp
+#pragma once
+#include <functional> // std::invoke
+#include <mpc/functional/perfect_forward.hpp>
+
+namespace mpc {
+  // monad_trans
+  // https://hackage.haskell.org/package/transformers-0.6.0.2/docs/Control-Monad-Trans-Class.html
+
+  /// class (forall m. Monad m => Monad (t m)) => MonadTrans t where
+  template <class>
+  struct monad_trans_traits;
+
+  template <class TR>
+  concept monad_trans = requires {
+    monad_trans_traits<std::remove_cvref_t<TR>>::lift;
+  };
+
+  namespace detail {
+    template <class TR>
+    using lift_op =
+      std::remove_cvref_t<decltype(alternative_traits<std::remove_cvref_t<TR>>::lift)>;
+
+    template <class TR>
+    struct lift_t : perfect_forward<lift_op<TR>> {
+      using perfect_forward<lift_op<TR>>::perfect_forward;
+    };
+  } // namespace detail
+
+  inline namespace cpo {
+    /// lift :: (Monad m) => m a -> t m a
+    template <class TR>
+    inline constexpr detail::lift_t<std::remove_cvref_t<TR>> lift{};
+  } // namespace cpo
+} // namespace mpc
