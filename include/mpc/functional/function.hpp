7e0ef05c1099018d37a2148ec8f99ffae20310cb
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-07 15:08:42 +0900

    ⚡️ Improve mpc::function

--- include/mpc/functional/function.hpp
+++ include/mpc/functional/function.hpp
@@ -5,10 +5,10 @@
 #include <mpc/stdfundamental.hpp>
 
 namespace mpc {
+  // https://stackoverflow.com/questions/53977787/constexpr-version-of-stdfunction
   template <class Ret, class Arg>
   struct _function {
     constexpr virtual ~_function() = default;
-    constexpr virtual Ret operator()(const Arg&) const = 0;
     constexpr virtual Ret operator()(Arg&&) const = 0;
   };
 
@@ -20,9 +20,6 @@ namespace mpc {
   public:
     constexpr _function_impl(const F& f) : f_(f) {}
     constexpr _function_impl(F&& f) : f_(std::move(f)) {}
-    constexpr Ret operator()(const Arg& arg) const override {
-      return std::invoke(f_, arg);
-    }
     constexpr Ret operator()(Arg&& arg) const override {
       return std::invoke(f_, std::move(arg));
     }
@@ -39,17 +36,20 @@ namespace mpc {
 
   public:
     template <class F>
+    requires std::invocable<std::decay_t<F>&, Arg> and std::same_as<std::invoke_result_t<std::decay_t<F>&, Arg>, Ret>
     constexpr function(F&& f)
       : instance_(std::make_shared<_function_impl<std::decay_t<F>, Ret, Arg>>(
         std::forward<F>(f))) {}
     constexpr Ret operator()(const Arg& arg) const {
-      return instance_->operator()(arg);
+      auto tmp = arg;
+      return instance_->operator()(std::move(tmp));
     }
     constexpr Ret operator()(Arg&& arg) const {
       return instance_->operator()(std::move(arg));
     }
     constexpr Ret operator%(const Arg& arg) const {
-      return instance_->operator()(arg);
+      auto tmp = arg;
+      return instance_->operator()(std::move(tmp));
     }
     constexpr Ret operator%(Arg&& arg) const {
       return instance_->operator()(std::move(arg));
