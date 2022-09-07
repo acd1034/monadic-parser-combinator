e1e26052377c8e97ff13fad3dd78b605b7686361
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-07 18:38:56 +0900

    ⚡️ std::string_view→std::list<char>

--- examples/draft2.cpp
+++ examples/draft2.cpp
@@ -3,11 +3,11 @@
 #include <string_view>
 #include <mpc/control.hpp>
 #include <mpc/data.hpp>
+#define MPC_FORWARD(x) std::forward<decltype(x)>(x)
 using namespace std::literals;
 using mpc::operators::alternatives::operator||;
 template <class Op, class... Args>
 using applicable = mpc::partially_applicable<Op, Args...>;
-
 template <class T>
 [[deprecated]] constexpr void type_of() {}
 
@@ -17,24 +17,22 @@ template <class T>
 // Test implementation of parser combinator
 // Reference: https://qiita.com/7shi/items/b8c741e78a96ea2c10fe#%E3%83%A2%E3%83%8A%E3%83%89%E5%A4%89%E6%8F%9B%E5%AD%90%E3%81%A7%E5%90%88%E6%88%90
 
-template <class V>
-using Monad = mpc::either<std::string, V>;
-
-using Result = std::pair<char, std::string_view>;
-
-using ST = mpc::StateT<std::string_view, mpc::either<std::string, Result>>;
+using String = std::list<char>;
+using Result = std::pair<char, String>;
+using ST = mpc::StateT<String, mpc::either<std::string, Result>>;
 
 template <mpc::is_StateT ST2>
 using StateT_value_in_monad_t =
   decltype(mpc::fmap(mpc::fst, std::declval<mpc::StateT_monad_t<ST2>>()));
 
-constexpr Result //
-decomp(std::string_view sv) {
-  return {sv.front(), sv.substr(1)};
+Result decomp(String&& str) {
+  auto c = str.front();
+  str.pop_front();
+  return {c, std::move(str)};
 }
 
-void parseTest(const std::size_t n, const auto& st, std::string_view sv) {
-  const auto result = mpc::eval_StateT % st % sv;
+void parseTest(const std::size_t n, const auto& parser, std::string_view sv) {
+  const auto result = mpc::eval_StateT % parser % std::list(sv.begin(), sv.end());
   if (result.index() == 0) {
     std::cout << n << " [" << sv << "] " << *std::get<0>(result) << std::endl;
   } else {
@@ -44,43 +42,50 @@ void parseTest(const std::size_t n, const auto& st, std::string_view sv) {
 
 template <class T>
 struct mpc::alternative_traits<mpc::either<std::string, T>> {
-  static constexpr auto combine = [](const auto& m1, const auto& m2) //
+  static constexpr auto combine = [](auto&& m1, auto&& m2) //
     -> mpc::either<std::string, T> {
     if (m1.index() == 0) {
       if (m2.index() == 0) {
-        return mpc::make_left(*std::get<0>(m1) + " AND " + *std::get<0>(m2));
+        return mpc::make_left(*std::get<0>(MPC_FORWARD(m1)) + " AND "
+                              + *std::get<0>(MPC_FORWARD(m2)));
       } else {
-        return m2;
+        return MPC_FORWARD(m2);
       }
     } else {
-      return m1;
+      return MPC_FORWARD(m1);
     }
   };
 };
 
 inline constexpr auto satisfy = //
   applicable([](std::predicate<char> auto pred) {
-    return mpc::make_StateT<std::string_view>(applicable(
-      [](auto pred, std::string_view sv) -> mpc::either<std::string, Result> {
-        if (std::ranges::empty(sv)) {
+    return mpc::make_StateT<String>(applicable(
+      [](auto pred, String str) -> mpc::either<std::string, Result> {
+        if (str.empty()) {
           return mpc::make_left("unexpected end of input"s);
-        } else if (auto [x, xs] = decomp(sv); not std::invoke(pred, x)) {
+        } else if (auto [x, xs] = decomp(std::move(str)); not std::invoke(pred, x)) {
           return mpc::make_left("unexpected "s + mpc::quoted(x));
         } else {
-          return mpc::make_right(std::make_pair(x, xs));
+          return mpc::make_right(std::make_pair(x, std::move(xs)));
         }
       },
       std::move(pred)));
   });
 
 inline constexpr auto left =
-  mpc::compose % mpc::lift<ST> % applicable([](const auto& str) -> StateT_value_in_monad_t<ST> {
-    return mpc::make_left(str);
-  });
+  mpc::compose
+  % mpc::lift<ST> % applicable([](/* std::string */ auto&& str) -> StateT_value_in_monad_t<ST> {
+      return mpc::make_left(MPC_FORWARD(str));
+    });
 
 inline constexpr auto char1 = applicable([](char c) {
-  return satisfy % (mpc::equal_to % c) or left % ("expecting char "s + mpc::quoted(c));
+  auto c2 = c;
+  return satisfy % (mpc::equal_to % std::move(c))
+         or left % ("expecting char "s + mpc::quoted(std::move(c2)));
 });
+
+// many p = ((:) <$> p <*> many p) <|> return []
+// inline constexpr auto many = applicable([](auto&& p){})
 
 int main() {
   const auto anyChar = satisfy % (mpc::constant % true);
