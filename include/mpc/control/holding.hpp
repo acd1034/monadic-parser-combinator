b92faf3439d48e13a1190e4d73fdacbe45592f17
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-07 19:49:54 +0900

    ✨ Add holding.hpp

---
+++ include/mpc/control/holding.hpp
@@ -0,0 +1,18 @@
+/// @file holding.hpp
+#pragma once
+#include <mpc/stdfundamental.hpp>
+
+namespace mpc {
+  template <class>
+  struct holding;
+
+  template <class T, class U>
+  struct holding_or : std::type_identity<U> {};
+
+  template <class T, class U>
+  requires requires { typename holding<std::remove_cvref_t<T>>::type; }
+  struct holding_or<T, U> : std::remove_cvref_t<T> {};
+
+  template <class T, class U>
+  using holding_or_t = typename holding_or<T, U>::type;
+} // namespace mpc
