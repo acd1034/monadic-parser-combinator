26bfde131e93844039750e50050059582f114758
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-24 20:13:49 +0900

    ✨ Add forward_like.hpp

--- include/mpc/utility.hpp
+++ include/mpc/utility.hpp
@@ -2,6 +2,7 @@
 #pragma once
 #include <mpc/utility/alternative_value_t.hpp>
 #include <mpc/utility/copyable_box.hpp>
+#include <mpc/utility/forward_like.hpp>
 #include <mpc/utility/overloaded.hpp>
 #include <mpc/utility/single.hpp>
 #include <mpc/utility/tuple_like.hpp>
