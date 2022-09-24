ffce38c2b676d15f3bd2b952646144005a408526
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-24 21:52:24 +0900

    ✨ Add test for parser

---
+++ tests/parser/parser.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_MAIN
+#include <algorithm>
+#include <catch2/catch.hpp>
+#include <mpc/parser.hpp>
+#include "../stdfundamental.hpp"
+using namespace std::string_view_literals;
+
+bool operator==(const mpc::String& x, const std::string_view& y) {
+  return std::equal(x.begin(), x.end(), y.begin(), y.end());
+}
+
+void CHECK_FAIL(mpc::similar_to<mpc::Parser> auto&& parser, std::string_view sv) {
+  auto result = mpc::eval_StateT % MPC_FORWARD(parser) % mpc::String(sv.begin(), sv.end());
+  CHECK(result.index() == 0);
+}
+
+template <class T>
+void CHECK_SUCCEED(mpc::similar_to<mpc::Parser> auto&& parser, std::string_view sv,
+                   const T& expected) {
+  auto result = mpc::eval_StateT % MPC_FORWARD(parser) % mpc::String(sv.begin(), sv.end());
+  CHECK(result.index() == 1);
+  CHECK(*mpc::snd(result) == expected);
+}
+
+TEST_CASE("parser min", "[parser][min]") {
+  CHECK_SUCCEED(mpc::satisfy % mpc::isdigit, "0", '0');
+}
