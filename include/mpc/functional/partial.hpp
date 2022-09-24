75bd9fa83788c668754d8bbc634e7d4102ee8450
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-09-24 20:00:03 +0900

    ⚡️ Unwrap partial

--- include/mpc/functional/partial.hpp
+++ include/mpc/functional/partial.hpp
@@ -3,16 +3,7 @@
 #include <functional> // std::invoke
 #include <mpc/utility/copyable_box.hpp>
 
-// clang-format off
-
 namespace mpc {
-  // partial
-  // https://github.com/llvm/llvm-project/blob/main/libcxx/include/__functional/perfect_forward.h
-
-  /// Implements a perfect-forwarding call wrapper.
-  template <copy_constructible_object Op, class... Bound>
-  struct partial;
-
   template <class T, class U>
   using __override_ref_t =
     std::conditional_t<std::is_rvalue_reference_v<T>, std::remove_reference_t<U>&&, U&>;
@@ -23,6 +14,51 @@ namespace mpc {
 
   template <class T, class U>
   using __forward_like_t = __override_ref_t<T&&, __copy_const_t<T, std::remove_reference_t<U>>>;
+
+  template <typename T>
+  [[nodiscard]] constexpr auto forward_like(auto&& x) noexcept -> __forward_like_t<T, decltype(x)> {
+    return static_cast<__forward_like_t<T, decltype(x)>>(x);
+  }
+} // namespace mpc
+
+// clang-format off
+
+namespace mpc {
+  // partial
+  // https://github.com/llvm/llvm-project/blob/main/libcxx/include/__functional/perfect_forward.h
+
+  /// Implements a perfect-forwarding call wrapper.
+  template <copy_constructible_object Op, class... Bound>
+  struct partial;
+
+  template <class T>
+  inline constexpr bool is_partial_v = false;
+
+  template <copy_constructible_object Op, class... Bound>
+  inline constexpr bool is_partial_v<partial<Op, Bound...>> = true;
+
+  template <class Op, class Tuple, std::size_t... Idx, class... Args>
+  constexpr auto __make_partial_impl(Op&& op, Tuple&& tuple, std::index_sequence<Idx...>, Args&&... args) noexcept(
+    noexcept(   partial(std::forward<Op>(op), std::get<Idx>(std::forward<Tuple>(tuple))..., std::forward<Args>(args)...)))
+    -> decltype(partial(std::forward<Op>(op), std::get<Idx>(std::forward<Tuple>(tuple))..., std::forward<Args>(args)...)) {
+    return      partial(std::forward<Op>(op), std::get<Idx>(std::forward<Tuple>(tuple))..., std::forward<Args>(args)...);
+  }
+
+  template <class Op, class... Args,
+            class = std::enable_if_t<is_partial_v<std::remove_cvref_t<Op>>>>
+  constexpr auto __make_partial(Op&& op, Args&&... args) noexcept(
+    noexcept(   __make_partial_impl(forward_like<Op>(op.op_), forward_like<Op>(op.bound_), std::make_index_sequence<std::tuple_size_v<decltype(op.bound_)>>(), std::forward<Args>(args)...)))
+    -> decltype(__make_partial_impl(forward_like<Op>(op.op_), forward_like<Op>(op.bound_), std::make_index_sequence<std::tuple_size_v<decltype(op.bound_)>>(), std::forward<Args>(args)...)) {
+    return      __make_partial_impl(forward_like<Op>(op.op_), forward_like<Op>(op.bound_), std::make_index_sequence<std::tuple_size_v<decltype(op.bound_)>>(), std::forward<Args>(args)...);
+  }
+
+  template <class Op, class... Args,
+            class = std::enable_if_t<!is_partial_v<std::remove_cvref_t<Op>>>>
+  constexpr auto __make_partial(Op&& op, Args&&... args) noexcept(
+    noexcept(   partial(std::forward<Op>(op), std::forward<Args>(args)...)))
+    -> decltype(partial(std::forward<Op>(op), std::forward<Args>(args)...)) {
+    return      partial(std::forward<Op>(op), std::forward<Args>(args)...);
+  }
 
   template <class Op, class Tuple, std::size_t... Idx, class... Args,
             class = std::enable_if_t<std::is_invocable_v<Op, __forward_like_t<Tuple, std::tuple_element_t<Idx, std::remove_cvref_t<Tuple>>>..., Args...>>>
@@ -45,6 +81,9 @@ namespace mpc {
   private:
     copyable_box<Op> op_{};
     std::tuple<Bound...> bound_{};
+
+    template <class Op2, class... Args, class>
+    friend constexpr auto __make_partial(Op2&&, Args&&...);
 
   public:
     constexpr explicit partial()
